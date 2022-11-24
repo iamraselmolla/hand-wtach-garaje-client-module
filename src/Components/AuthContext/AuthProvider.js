45483cced4f4221b03c74b192870a8778047c217
iamraselmolla <raselmolla6336@gmail.com>
2022-11-25 00:17:41 +0600

    added register

---
+++ src/Components/AuthContext/AuthProvider.js
@@ -0,0 +1,53 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../firebase/firebase-init';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // User registration
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    // Social login (Google)
+    const loginWithGoogle = (provicer) => {
+        return signInWithPopup(auth, provicer)
+    }
+    // User login
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // User Logout
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth);
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
+
+
+    const authInfo = { createUser, loginWithGoogle, login, logOut }
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
--- src/Components/Pages/Register/Register.js
+++ src/Components/Pages/Register/Register.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, {useContext, useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../AuthContext/AuthProvider';
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext);
+    const googleAuth = new GoogleAuthProvider();
+    const [error, setError] = useState('')
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+        const accountType = e.target.sellerBuyer.value
+        const username = e.target.username.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const profilepicture = e.target.profilepicture.value;
+        const insertTime = new Date().getTime();
+        const allData = {accountType, username, email, password, profilepicture, insertTime}
+        createUser(e.target.email.value, e.target.password.value)
+        .then( res => {
+            console.log(allData)
+        })
+        .catch(err => console.log(err.message));
+    }
     return (
         <section className='py-5 text-center'>
             <div className="container py-4">
@@ -14,25 +35,36 @@ const Register = () => {
                             <h1 className="fw-bolder mb-3">
                                 Register Now!
                             </h1>
-                            <Form className='text-start'>
-                                <Form.Group className="mb-3" controlId="formBasicEmail2">
+                            <Form onSubmit={handleRegister} className='text-start'>
+                                <h5 className="fw-bolder">
+                                    Account Type
+                                </h5>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="sellerBuyer" id="inlineRadio1" value="seller"/>
+                                        <label className="form-check-label" htmlFor="inlineRadio1">Seller</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="sellerBuyer" id="inlineRadio2" value="buyer"/>
+                                        <label className="form-check-label" htmlFor="inlineRadio2">Buyer</label>
+                                </div>
+                                <Form.Group className="mb-3 mt-4" controlId="formBasicEmail2">
                                     <Form.Label>Username</Form.Label>
-                                    <Form.Control type="text" className='rounded-5' placeholder="Username" />
+                                    <Form.Control name="username" type="text" className='rounded-5' placeholder="Username" />
 
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formBasicName2">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" className='rounded-5' placeholder="example@gmail.com" />
+                                    <Form.Control name="email" type="email" className='rounded-5' placeholder="example@gmail.com" />
 
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword2">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control className='rounded-5' type="password" placeholder="Password" />
+                                    <Form.Control name="password" className='rounded-5' type="password" placeholder="Password" />
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formBasicFile2">
                                     <Form.Label>Profile Picture</Form.Label>
-                                    <Form.Control className='rounded-5' type="file" placeholder="Profile Picture" />
+                                    <Form.Control name="profilepicture" className='rounded-5' type="file" placeholder="Profile Picture" />
                                 </Form.Group>
 
                                 <button className="theme_bg outline-0 border-0 px-5 py-3 fw-bolder text-white rounded">
@@ -41,8 +73,8 @@ const Register = () => {
 
                             </Form>
                             <div className="d-flex mt-2">
-                               Already Registered? <Link className='text-decoration-none ms-2 theme_color fw-bolder' to="/login">Login</Link>
-                               
+                                Already Registered? <Link className='text-decoration-none ms-2 theme_color fw-bolder' to="/login">Login</Link>
+
                             </div>
                         </div>
                     </div>

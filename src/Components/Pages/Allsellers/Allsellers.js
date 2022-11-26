ed5900a34f1f68b6065780e8707241ac96213820
iamraselmolla <raselmolla6336@gmail.com>
2022-11-26 12:00:05 +0600

    displaying all seller account in dashboard

--- src/Components/Pages/Allsellers/Allsellers.js
+++ src/Components/Pages/Allsellers/Allsellers.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../AuthContext/AuthProvider';
+import Account from '../Shared/Account';
+import Table from 'react-bootstrap/Table';
 
 const Allsellers = () => {
+    const { user } = useContext(AuthContext)
+    const [accounts, setAccounts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/accounts?account=seller')
+            .then(res => res.json())
+            .then(data => setAccounts(data))
+            .catch(err => console.log(err.message))
+    }, [user?.email])
     return (
-        <div>
-            
-        </div>
+        <>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Profile Picture</th>
+                    <th>Account Type</th>
+                    <th>Signup by</th>
+                    <th>Signup at</th>
+                </tr>
+            </thead>
+            <tbody>
+               {accounts?.map((acc,i) =>  <Account index={i}  acc ={acc} key={acc._id}></Account>)}
+
+            </tbody>
+        </Table>
+    </>
     );
 };
 

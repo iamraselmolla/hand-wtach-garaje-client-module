e186ee50d9e452d9ae264cdebb0f8ac09b56f94e
iamraselmolla <raselmolla6336@gmail.com>
2023-01-02 00:44:08 +0600

    added payment verification method

--- src/Components/Pages/AllUploadedItems/AllUploadedItems.js
+++ src/Components/Pages/AllUploadedItems/AllUploadedItems.js
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Table } from 'react-bootstrap';
-import Items from '../Shared/Items';
-import ItemsTable from '../Shared/ItemsTable';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { Link } from 'react-router-dom';
 
 const AllUploadedItems = () => {
-    const { data: allWatches = [], isLoading, refetch } = useQuery({
+    const { data: allWatches = [] } = useQuery({
         queryKey: ['all-items'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/all-uploaded-items');
@@ -13,60 +13,66 @@ const AllUploadedItems = () => {
             return data;
         }
     });
-
+    let homeURL = window.location.host;
     return (
         <section>
-            <div className="container-fluid py-5">
+            <div className="container-fluid">
                 <div className="row text-center mb-4">
                     <h1 className="fw-bolder theme_color">
                         We had {allWatches?.length} total {allWatches?.length > 1 ? 'Watches' : 'Watch'}
                     </h1>
                 </div>
                 <>
-            {allWatches?.length > 0 ?
-                <section>
-                    <div className="container">
-                        <div className="row text-center">
-                            <Table striped bordered hover>
-                                <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Watch Name</th>
-                                        <th>Price</th>
-                                        <th>Image</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-
-                                    {allWatches?.map((s, i) => {
-                                        return <>
-
-                                            <tr className={`${s?.sold ? 'bg-danger text-white' : ''}`}>
-                                                <td>{i + 1}</td>
-                                                <td> {s?.name} </td>
-                                                <td> {s?.price} </td>
-                                                <td> <img src={s?.itemImage} width="50px" className='rounded-cricle' alt="" srcset="" /> </td>
-                                                <th> {s?.sold ? 'Sold' : 'Unsold'} </th>
+                    {allWatches?.length > 0 ?
+                        <section>
+                            <div className="container">
+                                <div className="row text-center">
+                                    <Table bordered hover>
+                                        <thead>
+                                            <tr className='bg-black text-white fs-5'>
+                                                <th></th>
+                                                <th>Watch Name</th>
+                                                <th>Price</th>
+                                                <th>Image</th>
+                                                <th>Status</th>
                                             </tr>
+                                        </thead>
+                                        <tbody>
 
 
-                                        </>
-                                    })}
+                                            {allWatches?.map((s, i) => {
+                                                return <>
 
-                                </tbody>
-                            </Table>
+                                                    <tr className={`${s?.sold ? 'bg-danger text-white' : 'bg-primary text-white'}`}>
+                                                        <td>{i + 1}</td>
+                                                        <td className='text-white'> <Link to={`/details/items/${s?._id}`} className='text-decoration-none text-white'>  {s?.name.split(' ').length > 6 ? s?.name.split(' ').slice(0, 5) + '...' : s?.name} </Link></td>
+                                                        <td> {s?.price} </td>
+                                                        <td> <PhotoProvider>
+                                                            <div className="foo">
+                                                                <PhotoView src={s?.itemImage}>
+                                                                <img src={s?.itemImage} width="50px" className='rounded-cricle' alt="Watch image"/>
+                                                                </PhotoView>
+                                                            </div>
+                                                        </PhotoProvider> </td>
+                                                        <th> {s?.sold ? 'Sold' : 'Unsold'} </th>
+                                                    </tr>
 
-                        </div>
-                    </div>
-                </section>
-                : <h2 className="fw-bolder text-center text-success">
-                    You have no reported items
-                </h2>
-            }
 
-        </>
+                                                </>
+                                            })}
+
+                                        </tbody>
+                                    </Table>
+
+                                </div>
+                            </div>
+                        </section>
+                        : <h2 className="fw-bolder text-center text-success">
+                            You have no reported items
+                        </h2>
+                    }
+
+                </>
             </div>
 
         </section>
--- src/Components/Pages/Verification/Verification.js
+++ src/Components/Pages/Verification/Verification.js
@@ -1,26 +1,41 @@
 import { async } from '@firebase/util';
+import { PaymentElement } from '@stripe/react-stripe-js';
 import { useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react';
 import { Button, Form, Spinner } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Verification = () => {
     const [loader, setLoader] = useState(false)
-    const [paymentData, setPaymentData] = useState({})
+    const [notFound, setNotFound] = useState()
+    const [paymentData, setPaymentData] = useState();
     const handleVerify = async (e) => {
-        setLoader(true)
-        e.preventDefault();
-        const res = await fetch(`http://localhost:5000/payment-verification?paymentID=${e.target.paymentid.value}`);
-        const newPaymentData = await res.json();
-        setLoader(false)
-        return setPaymentData(newPaymentData);
+        try {
+            setPaymentData(null)
+            setLoader(true)
+            e.preventDefault();
+            const res = await fetch(`http://localhost:5000/payment-verification?paymentID=${e.target.paymentid.value}`);
+            const newPaymentData = await res.json();
+
+            setLoader(false)
+            console.log(newPaymentData);
+            e.target.reset()
+            setNotFound(null)
+            return setPaymentData(newPaymentData);
+        } catch {
+            setLoader(false)
+            setNotFound('Data not found. Please check it again')
+        }
+        finally{
+            setLoader(false)
+        }
     }
-   
-    console.log(paymentData)
+
     return (
         <section>
             <div className="container py-5">
                 <div className="row">
-                    
+
                     <div className="col-md-3 mx-auto">
                         <Form onSubmit={handleVerify}>
                             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -28,11 +43,64 @@ const Verification = () => {
                                 <Form.Control name="paymentid" type="text" placeholder="Payment ID" />
                             </Form.Group>
                             <button type='submit' className="theme_bg w-100 fw-bolder text-white border-0 rounded py-2 px-2">
-                                Verify
+                                {loader ? 'Verifying' : 'Verify'}
                             </button>
                         </Form>
                     </div>
                 </div>
+                {paymentData &&
+                    <div className="row mt-4">
+                        <div className="col-md-4">
+                            <>
+                                <h2 className="fw-bold mb-4">
+                                    Product Information
+                                </h2>
+                                <img className='img-fluid' style={{ maxHeight: '300px' }} src={paymentData?.img} alt="" />
+                                <h3 className="mt-3">
+                                    <Link to={`/details/items/${paymentData?.product_id}`} className="text-decoration-none"><b>Product Name:</b> {paymentData?.productname}</Link>
+                                </h3>
+                            </>
+
+
+                        </div>
+                        <div className="col-md-4">
+                            <h2 className="fw-bold mb-4">
+                                Payment Information
+                            </h2>
+                            <h5 className="fw-bolder theme_color">
+                                Price: {paymentData?.price}$
+                            </h5> 
+                            <p className="mb-2">
+                                <b>Card: </b> {paymentData?.payment_type}
+                            </p>
+                            <p className="mb-2">
+                                <b>Booked At: </b> {new Date(paymentData?.insertTime).toLocaleString()}
+                            </p>
+                            <p className="mb-2">
+                                <b>Payment At: </b> {new Date(paymentData?.paymentTime).toLocaleString()}
+                            </p>
+                        </div>
+                        <div className="col-md-4">
+                            <h2 className="fw-bold mb-4">
+                                Customer Information
+                            </h2>
+                            <h3>
+                                <b>Name:</b> {paymentData?.name}
+                            </h3>
+                            <p className="mb-2">
+                                <b>Email: </b> {paymentData?.email}
+                            </p>
+                            <p className="mb-2">
+                                <b>Phone: </b> {paymentData?.number}
+                            </p>
+                            <p className="mb-2">
+                                <b>Meeting Location: </b> {paymentData?.location}
+                            </p>
+
+                        </div>
+                        {notFound && <p className='text-danger fs-5 fw-bolder'>{notFound}</p>}
+                    </div>
+                }
             </div>
         </section>
     );
